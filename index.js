3c10c515ca96759210652002a2f47c68558b6872
Lokinsky Vladimir <wf.pbvova2030@icloud.com>
2019-10-23 19:28:43 +0300

    the bold dot

--- index.js
+++ index.js
@@ -66,7 +66,8 @@ server.get('/',(req,res)=>{
 
 server.get('/main',(req,res,)=>{
     console.log(req.ip)
-    res.sendFile(__dirname+'/public/html/main.html',(err)=>{});
+    res.setHeader("Connection","close connection")
+    res.sendFile(__dirname+'/public/html/main.html',(err)=>{res.end()});
 
 });
 
